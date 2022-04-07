956ef4f8c1cee8b8de66d55605091d8253aed133
Tim Chen Yu-Ting <tim.chen@titansoft.com.sg>
2022-04-08 00:47:18 +0800

    update crawler by using C# plug-in AngleSharp

---
+++ Crawler/Crawler/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Crawler.Models;
+using AngleSharp;
+
+namespace Crawler.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var config = Configuration.Default.WithDefaultLoader();
+        var context = BrowsingContext.New(config);
+        var document = await context.OpenAsync("https://calendar.google.com/calendar/u/1/r");
+        var contents = document.QuerySelectorAll(".content");
+        return View(contents);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
